807a4433d1f994c72584fb2815cf1486451c00c8
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-30 18:17:53 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop()
+end
+
+def pop_with_args(array)
+  return array.pop(2)
+end
+
+def using_shift(array)
+  return array.shift()
+end
+
+def shift_with_args(array)
+  return array.shift(2)
+end
+
+  
